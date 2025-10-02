5a119a09a43d37281580b1d6a4b07a8255b801d0
Cratairx <daniel.inserte@gmail.com>
2025-10-02 14:46:11 +0200

    lagt till klasser i övning uppgift 3

---
+++ src/OOPUppgift3/Person.java
@@ -0,0 +1,27 @@
+package OOPUppgift3;
+
+public class Person {
+    String name;
+    String personalId;
+
+    public Person(String name, String personalId) {
+        this.name = name;
+        this.personalId = personalId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPersonalId() {
+        return personalId;
+    }
+
+    public void setPersonalId(String personalId) {
+        this.personalId = personalId;
+    }
+}
--- src/OOPUppgift3/Teacher.java
+++ src/OOPUppgift3/Teacher.java
@@ -1,4 +1,13 @@
 package OOPUppgift3;
 
 public class Teacher {
+    protected String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
