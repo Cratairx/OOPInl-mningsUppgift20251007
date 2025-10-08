8aeb6c2a659ecbd52f0ed8207cf9303681ac273d
Cratairx <126484916+Cratairx@users.noreply.github.com>
2025-10-08 19:15:11 +0200

    Klar

--- src/InlamningsUppgiftGreenest/MainMenu/MainMenuChoiceZero.java
+++ src/InlamningsUppgiftGreenest/MainMenu/MainMenuChoiceZero.java
@@ -15,9 +15,8 @@ public class MainMenuChoiceZero {
             Scanner scanner = new Scanner(System.in);
             System.out.printf("Hur stor är Laura? ");
             input = scanner.nextDouble();
-          palmenLaura.getPalmenLaura().printMe();// polymorfism kallar på printMe i palm
+            palmenLaura.getPalmenLaura().printMe();// polymorfism kallar på printMe i palm
             System.out.println("Jag behöver ha " + palmenLaura.getPalmenLaura().getWateringNeeds(input) + " Liter " + palmenLaura.getPalmenLaura().getLiquidSort().liquidSorts + " om dagen");
-
 
         }catch (InputMismatchException e){
             System.out.println("Du måste skriva en siffra i meter.");
