440d7b3af93bb510cf15e20d2393f318c53604d9
Cratairx <126484916+Cratairx@users.noreply.github.com>
2025-10-14 06:44:01 +0200

    fyllt på övningsuppgifter

---
+++ src/OvningsUppgifter_Sprint2/Car.java
@@ -0,0 +1,37 @@
+package OvningsUppgifter_Sprint2;
+
+public class Car {
+
+    private double milePerYear;
+    private double todaysMilage;
+    private double literUsagePerMile;
+
+    public Car(double milePerYear, double todaysMilage, double literUsagePerMile) {
+        this.milePerYear = milePerYear;
+        this.todaysMilage = todaysMilage;
+        this.literUsagePerMile = literUsagePerMile;
+    }
+
+    public double getMilePerYear() {
+        return milePerYear;
+    }
+
+    public double getTodaysMilage() {
+        return todaysMilage;
+    }
+
+    public double getLiterUsagePerMile() {
+        return literUsagePerMile;
+    }
+
+    public double getMilagePerYear(){
+        return getMilePerYear();
+    }
+
+    public double getAvgMilagePerYear(){
+
+        return (getMilagePerYear() + getLiterUsagePerMile())/ 2;
+
+    }
+
+}
